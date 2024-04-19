a7b75d3bfce7d010155096e8096e7313785bb65c
arifulla <arifulla.lab01@gmail.com>
2024-04-19 17:52:38 +0530

    added the all

---
+++ Basics/Indententation.py
@@ -0,0 +1,6 @@
+number = 10
+remainder = number % 2
+if remainder == 0:
+    print("even")
+else:
+    print("odd")
---
+++ Basics/conditional.py
@@ -0,0 +1,13 @@
+num = 15
+if num >=0:
+    print("im in if")
+    if num == 0:
+        print("Zero")
+        print("im in if => if")
+    else:
+        print("Positive number")
+        print("im in if => else")
+else:
+    print("Negative number")
+    print("im in else")
+print("im outside")
